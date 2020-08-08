fe23fe2fbfa7202cea4c2dd171b249d104e045b6
vincent105 <vincent1050917@gmail.com>
2020-08-08 17:08:42 +0800

    0808

---
+++ WebSite1/WebSite1/ch2/ch2_6_1_for.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ch2_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int my_sum = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            my_sum = my_sum + i;
+            Response.Write("a" + i + ":" + i + "<br>");
+        }
+        Response.Write("1+...+100=" + my_sum);
+    }
+}
