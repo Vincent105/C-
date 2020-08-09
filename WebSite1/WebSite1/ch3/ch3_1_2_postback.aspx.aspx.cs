46dc107718b9db5c553a66359a337b06ea668b18
vincent105 <vincent1050917@gmail.com>
2020-08-09 12:19:31 +0800

    0809

---
+++ WebSite1/WebSite1/ch3/ch3_1_2_postback.aspx.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ch3_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int i = Convert.ToInt32(Label3.Text) + 1;
+        Label3.Text = i.ToString();
+    }
+
+}
