516dd4b88d380108e7c60a464dd3cf63e63a58f0
vincent105 <vincent1050917@gmail.com>
2020-08-23 10:31:52 +0800

    0823

---
+++ firstMVC/firstMVC/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace firstMVC.Models
+{
+    public class Class1
+    {
+        public int id { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
