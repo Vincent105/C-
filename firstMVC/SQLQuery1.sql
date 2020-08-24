f9dbba179f6e8db60c1fd2502573694f191b1bc0
vincent105 <vincent1050917@gmail.com>
2020-08-24 21:51:43 +0800

    .

---
+++ firstMVC/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [subject] NVARCHAR(50) NOT NULL, 
+    [content] NVARCHAR(MAX) NOT NULL, 
+    [PostDateTime] DATETIME NOT NULL
+)
