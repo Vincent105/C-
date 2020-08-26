05b1c3de77ea04f1c0800407154cb538b354da22
Vincent105 <vincent1050917@gmail.com>
2020-08-26 13:04:08 +0800

    .

--- C70/chapter01/Listing01.01.HelloWorld.cs
+++ C70/chapter01/Listing01.01.HelloWorld.cs
@@ -2,12 +2,26 @@
 
 namespace chapter01
 {
-    public class helloWorld
+    public class HelloWorld
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello, My name is Vincent");
+            System.Console.WriteLine("Up");System.Console.WriteLine("Down");
             Console.WriteLine("Hello World1!");
-            Console.WriteLine("Hello World2!");
+            System.Console.WriteLine(
+                "Hello, Your name is Esther.");
+
+            //1.3使用變數
+            string max, min; 
+
+            max = "test1";
+            System.Console.WriteLine(max);
+
+            min = "test2";
+            System.Console.WriteLine(min);
         }
+
     }
+
 }
