247e65c0815e936c3e23f8b79224d39e70c04126
vincent105 <vincent1050917@gmail.com>
2020-08-26 21:29:29 +0800

    .

--- C70/chapter01/Listing01.14.Readline.cs
+++ C70/chapter01/Listing01.14.Readline.cs
@@ -19,8 +19,10 @@ namespace chapter01
             lastname = System.Console.ReadLine();
 
             System.Console.WriteLine("Your name is " + firstname + lastname);
+            System.Console.WriteLine("字串插值如下");
             System.Console.WriteLine($"Your name is {firstname}{lastname}");
-            System.Console.WriteLine("Your full name is {0}{1}.", firstname, lastname);
+            System.Console.WriteLine("複合格式化如下");
+            System.Console.WriteLine("Your full name is {0}{1}{1}{0}.", firstname, lastname);
 
         }
     }
