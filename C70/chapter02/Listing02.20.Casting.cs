bdf580fcbed79165a615f56eb78b8b13c0b1e3dc
Vincent105 <vincent1050917@gmail.com>
2020-08-28 17:55:49 +0800

    0828

--- C70/chapter02/Listing02.20.Casting.cs
+++ C70/chapter02/Listing02.20.Casting.cs
@@ -4,12 +4,25 @@ using System.Text;
 
 namespace chapter02
 {
-    class Class1
+    class Class0220
     {
-        static void Main() 
+        static void Main0220() 
         {
-            long longNumber = 50918309109;
-            int intNumber = (int)longNumber;
+            unchecked 
+            {
+                long longNumber = 50918309109;
+                int intNumber = (int)longNumber;
+
+                int n = int.MaxValue;
+                n = n + 1;
+
+                System.Console.WriteLine("整數值溢位:" + n);
+            }
+
+            int intNumber2 = 31416;
+            long longNumber2 = (long) intNumber2;
+
+            System.Console.WriteLine("隱式轉型:" + longNumber2);
 
         }
     }
---
+++ C70/chapter02/Listing02.26.Casting2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chapter02
+{
+    class Class0226
+    {
+        static void Main()
+        {
+            string text = "9.11E-31";
+            float kg = float.Parse(text);
+
+            System.Console.WriteLine("使用Parse()進行轉換型別:" + kg);
+
+            string middleText = "261.626";
+            double middleC = System.Convert.ToDouble(middleText);
+            bool boolen = System.Convert.ToBoolean(middleC);
+
+            System.Console.WriteLine(middleC + "   " + boolen);
+
+            bool boolean = true;
+            string text1 = boolean.ToString();
+            System.Console.WriteLine("使用ToString()進行轉換型別:" + text1);
+
+            string input;
+
+            System.Console.Write("Enter a number:");
+            input = System.Console.ReadLine();
+            if (Double.TryParse(input, out double number))
+            {
+                System.Console.WriteLine($"Your key-in is the {input}.");
+            }
+            else
+            {
+                System.Console.WriteLine("not a valid number");
+            }
+        }
+    }
+}
