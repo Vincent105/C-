ef3f6233730c8ae9d29c4eb97ae90d36f373b29b
Vincent105 <vincent1050917@gmail.com>
2020-09-01 17:14:40 +0800

    .

---
+++ MvcMicro/MvcMicro/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMicro.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b>default</b> page...";
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return "This is <b>welcome</b> method...";
+            return HttpUtility.HtmlEncode("Hello",);
+        }
+    }
+}
---
+++ MvcMicro/MvcMicro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcMicro.MvcApplication" Language="C#" %>
