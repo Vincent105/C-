f36c601575d5c95ace2bbff29612d9370d72b2c5
vincent105 <vincent1050917@gmail.com>
2020-09-03 20:54:07 +0800

    .

---
+++ C70/chapter04/Listing04.45.for.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chapter04
+{
+    class Class0445
+    {
+        static void Main0445() 
+        {
+            for (int x = 0, y = 5; ((x <= 5) && ( y >= 0)); y-- , x++ )
+            {
+                System.Console.WriteLine($"{x}{((x>y) ? '>' : '<')}{y}");
+            }
+
+
+        }
+
+    }
+}
---
+++ C70/chapter04/Listing04.47.foreach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chapter04
+{
+    class Class0447
+    {
+        static void Main()
+        {
+            char[] cells = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            System.Console.WriteLine("The available moves as follows:");
+            foreach (char cell in cells)
+            {
+                if (cell != '0' || cell != 'x')
+                {
+                    System.Console.WriteLine($"{cell}");
+                }
+            }
+        }
+    }
+}
