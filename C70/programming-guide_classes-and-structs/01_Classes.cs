99218415365aa136157d26bc4d81f2900deb3c0b
vincent105 <vincent1050917@gmail.com>
2020-09-08 23:43:24 +0800

    .

---
+++ C70/programming-guide_classes-and-structs/01_Classes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace programming_guide_classes_and_structs
+{
+    public class Person01
+    {
+        public Person01()
+        {
+            Name = "Unknow";
+        }
+
+        public Person01(string name)
+        {
+            Name = name;
+        }
+        public string Name {get;}
+
+        // Method that overrides the base class (System.Object) implementation.
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    class TestPerson
+    {
+        static void Main01()
+        {
+            var person1 = new Person01();
+            Console.WriteLine(person1.Name);
+
+            Person01 person2 = new Person01("Vincent");
+            Person01 person3 = person2;
+            Console.WriteLine(person2.Name);
+
+            Console.WriteLine(person2);
+            Console.WriteLine(person3);
+            Console.WriteLine("Please any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C70/programming-guide_classes-and-structs/02_Objects1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace programming_guide_classes_and_structs
+{
+    class Person02
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person02(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+    }
+
+    class Program02
+    {
+        static void Main()
+        {
+            Person02 person1 = new Person02("Vincent", 30);
+            Console.WriteLine("person1 name is {0} and age is {1}", person1.Name, person1.Age);
+
+            Person02 person2 = person1;
+            person2.Name = "Esther";
+            person2.Age = 20;
+
+            Console.WriteLine("person1 name is {0} and age is {1}", person1.Name, person1.Age);
+            Console.WriteLine("person2 name is {0} and age is {1}", person2.Name, person2.Age);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
