cf55c68f628b196fa2afb8cbcb6feedda7ca5198
vincent105 <vincent1050917@gmail.com>
2020-09-18 20:37:52 +0800

    .

---
+++ MP32007/Chapter 2_Condition/WindowsFormsApp01_IF/WindowsFormsApp01_IF/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp01_IF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age;
+
+            age = Convert.ToInt32(textBox1.Text);
+
+            if (age <= 18)
+            {
+                MessageBox.Show("未滿18歲不能購買酒精");
+            }
+
+        }
+    }
+}
