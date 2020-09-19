b2000131ebac1bc4c6d8540c28116515239449c8
vincent105 <vincent1050917@gmail.com>
2020-09-19 22:31:43 +0800

    .

---
+++ MP32007/Chapter 2_Condition/WindowsFormsApp02_IF_Else/WindowsFormsApp02_IF_Else/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp02_IF_Else
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age;
+            age = Convert.ToInt32(textBox1.Text);
+
+            if (age <= 18)
+            {
+                MessageBox.Show("未滿18歲不能買酒");
+            }
+            else
+            {
+                label2.Visible = true;
+                textBox2.Visible = true;
+                button2.Visible = true;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number;
+            number = Convert.ToInt32(textBox2.Text);
+
+            label4.Text = (number * 32).ToString();
+        }
+    }
+}
---
+++ MP32007/Chapter 2_Condition/WindowsFormsApp03_overshape/WindowsFormsApp03_overshape/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp03_overshape
+{
+    public partial class Form1 : Form
+    {
+        int move = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (ovalShape1.Left <= 0)
+                move = -move;
+            if (ovalShape1.Left + ovalShape1.Width >= ClientSize.Width)
+                move = -move;
+            ovalShape1.Left += move;
+        }
+    }
+}
