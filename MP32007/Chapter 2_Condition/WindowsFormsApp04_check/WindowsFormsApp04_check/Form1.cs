d74ed9a2ff1aa6b4eb381b8bdfeb07cbea0c5f12
vincent105 <vincent1050917@gmail.com>
2020-09-20 22:11:32 +0800

    .

---
+++ MP32007/Chapter 2_Condition/WindowsFormsApp04_check/WindowsFormsApp04_check/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp04_check
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age;
+            age = Convert.ToInt32(textBox1.TabIndex);
+
+            if (age < 18)
+            {
+                MessageBox.Show("未滿18歲不可購買");
+            }
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
