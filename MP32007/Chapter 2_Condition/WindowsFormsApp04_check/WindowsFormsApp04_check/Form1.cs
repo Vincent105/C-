669206b937688fad49047017c71459538a135f07
Vincent105 <vincent1050917@gmail.com>
2020-09-21 09:06:43 +0800

    .

--- MP32007/Chapter 2_Condition/WindowsFormsApp04_check/WindowsFormsApp04_check/Form1.cs
+++ MP32007/Chapter 2_Condition/WindowsFormsApp04_check/WindowsFormsApp04_check/Form1.cs
@@ -30,7 +30,19 @@ namespace WindowsFormsApp04_check
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (e.KeyChar == (char)48 || e.KeyChar == (char)49 ||
+                e.KeyChar == (char)50 || e.KeyChar == (char)51 ||
+                e.KeyChar == (char)52 || e.KeyChar == (char)53 ||
+                e.KeyChar == (char)54 || e.KeyChar == (char)55 ||
+                e.KeyChar == (char)56 || e.KeyChar == (char)57 ||
+                e.KeyChar == (char)13 || e.KeyChar == (char)8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
         }
     }
 }
