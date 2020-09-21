1b5a7bdc905348cb8da85b2987aff9684680859e
vincent105 <vincent1050917@gmail.com>
2020-09-21 20:14:50 +0800

    .

---
+++ MP32007/Chapter 2_Condition/WindowsFormsApp05_trycatch/WindowsFormsApp05_trycatch/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp05_trycatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age;
+            try
+            {
+                age = Convert.ToInt32(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.ToString());
+                textBox1.Text = "";
+                return;
+            }
+
+            if (age < 18)
+            {
+                MessageBox.Show("未滿18歲不能購買");
+            }
+        }
+    }
+}
