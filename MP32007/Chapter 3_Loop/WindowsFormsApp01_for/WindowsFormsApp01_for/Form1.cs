505e9267b6da8055d5535ae44d8f9c924e0a05cf
vincent105 <vincent1050917@gmail.com>
2020-09-22 21:04:08 +0800

    .

---
+++ MP32007/Chapter 3_Loop/WindowsFormsApp01_for/WindowsFormsApp01_for/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp01_for
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            string str;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                str = "第" + i.ToString() + "次:" + sum.ToString() + "+" + i.ToString() + "=" + (sum + i).ToString();
+                textBox1.AppendText(str + "\r\n");
+                sum += i;
+            }
+            label2.Text = sum.ToString();
+        }
+    }
+}
