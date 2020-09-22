902bee16295a08bc8e905648148960db20b454d4
Vincent105 <vincent1050917@gmail.com>
2020-09-22 15:36:46 +0800

    .

---
+++ MP32007/Chapter 2_Condition/WindowsFormsApp08_switchCase/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp08_switchCase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string bankNo;
+
+            if (e.KeyChar == 13)
+            {
+                bankNo = textBox1.Text;
+
+                switch (bankNo)
+                {
+                    case "816":
+                        label3.Text = "安泰銀行";
+                        break;
+                    case "882":
+                        label3.Text = "中國信託";
+                        break;
+                    case "999":
+                        label3.Text = "測試銀行";
+                        break;
+                    default:
+                        MessageBox.Show("查無此銀行");
+                        label3.Text = "";
+                        break;
+                    
+                }
+                label3.Visible = true;
+            }
+
+        }
+    }
+}
