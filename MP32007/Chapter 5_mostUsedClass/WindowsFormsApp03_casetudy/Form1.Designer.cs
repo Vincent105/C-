63c4c3a5080b470e6f8f7ee84ac86464e01cc912
Vincent105 <vincent1050917@gmail.com>
2020-09-23 17:59:08 +0800

    .

---
+++ MP32007/Chapter 5_mostUsedClass/WindowsFormsApp03_casetudy/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp03_casetudy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "d0.png");
+            this.imageList1.Images.SetKeyName(1, "d1.png");
+            this.imageList1.Images.SetKeyName(2, "d2.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "h0.png");
+            this.imageList2.Images.SetKeyName(1, "h1.png");
+            this.imageList2.Images.SetKeyName(2, "h2.png");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(83, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 156);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(405, 93);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(134, 156);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(597, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "目前按鍵";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 353);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MP32007/Chapter 5_mostUsedClass/WindowsFormsApp03_casetudy/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp03_casetudy
+{
+    public partial class Form1 : Form
+    {
+        bool fgKeyUp = true;
+        int dBlood = 100, hBlood = 100;
+        int dState = 0, hState = 0; //0:放鬆 1:防守 2:攻擊
+        Random rd;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            rd = new Random();
+
+            pictureBox1.Image = imageList1.Images[0];
+            pictureBox2.Image = imageList2.Images[0];
+
+            label1.Text = dBlood.ToString();
+            label2.Text = hBlood.ToString();
+
+            this.Width = 800;
+            this.Height = 400;
+            pictureBox1.Left = 100;
+            pictureBox2.Left = 500;
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string str;
+            str = (e.KeyChar).ToString().ToUpper();
+            label4.Text = str;
+
+            //判斷遊戲是否結束
+            if (dBlood <= 0 || hBlood <= 0)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (fgKeyUp == true)
+            {
+                if (str == "A")
+                {
+                    pictureBox2.Image = imageList2.Images[2];
+                    fgKeyUp = false;
+                    hState = 2;
+                }
+                else
+                {
+                    pictureBox2.Image = imageList2.Images[1];
+                    fgKeyUp = false;
+                    hState = 1;
+                }
+
+                //判斷狀態與得分
+                if (dState == 2 && (hState == 0 || hState == 2))
+                {
+                    hBlood -= 10;
+                }
+
+                if (dState == 0 && hState == 2)
+                {
+                    dBlood -= 10;
+                }
+
+                label1.Text = dBlood.ToString();
+                label2.Text = hBlood.ToString();
+
+                //判斷win or lose
+                if (d)
+            }
+        }
+    }
+}
