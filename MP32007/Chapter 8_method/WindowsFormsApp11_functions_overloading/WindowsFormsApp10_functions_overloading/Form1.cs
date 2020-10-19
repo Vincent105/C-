c88bba39f74b28641a8f3de90bb2f04ce28c2097
vincent105 <vincent1050917@gmail.com>
2020-10-19 21:14:32 +0800

    1019

---
+++ MP32007/Chapter 8_method/WindowsFormsApp11_functions_overloading/WindowsFormsApp10_functions_overloading/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10_functions_overloading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "兩數相加=" + add(3, 5).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "兩數相加=" + add(4.2, 3.6).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "兩數相加=" + add(7, 2.6).ToString();
+        }
+
+        int add(int a, int b)
+        {
+            label2.Text = "呼叫第一個函式";
+            return a + b;
+        }
+        double add(double a, double b)
+        {
+            label2.Text = "呼叫第二個函式";
+            return a + b;
+        }
+        double add(int a, double b)
+        {
+            label2.Text = "呼叫第三個函式";
+            return a + b;
+        }
+    }
+}
---
+++ MP32007/Chapter 8_method/WindowsFormsApp12_generic1/WindowsFormsApp11_generic1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11_generic1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void exchange<T>(ref T a, ref T b)
+        {
+            T tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int a = 5, b = 8;
+            //exchange<int>(ref a, ref b);
+
+            double a = 5, b = 8;
+            exchange<double>(ref a,ref b);
+            label1.Text = string.Format("a={0} b={1}", a, b);
+        }
+    }
+}
