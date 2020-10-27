5d525855cf1eb415755746c7f7e77e3d7595e2f3
vincent105 <vincent1050917@gmail.com>
2020-10-27 21:15:28 +0800

    1027

---
+++ MvcMicro/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b>default</b> action";
+        }
+
+        /* CASE 1 : 預設 方法
+        public string Welcome()
+        {
+            return "This is the Welcome method";
+        }
+        */
+
+        // CASE 2 :  傳遞網址參數
+        /*
+        public string Welcome(string name , int numTimes=1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ",NumTimes is: " + numTimes);
+        }
+        */
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ",ID:" + ID);s
+        }
+
+    }
+}
