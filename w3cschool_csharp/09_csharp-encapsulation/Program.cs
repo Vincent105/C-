3487b6708e607190f6efd77b14f246215be7edbe
Vincent105 <vincent1050917@gmail.com>
2021-10-12 17:42:37 +0800

    1012

---
+++ w3cschool_csharp/09_csharp-encapsulation/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_csharp_encapsulation
+{
+    class ExcuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle_public r = new Rectangle_public();
+            r.length = 4;
+            r.width = 3;
+            r.Display();
+            Console.WriteLine("===Rectangle_public=========");
+
+            Rectangle_private r1 = new Rectangle_private();
+            r1.Acceptdetails();
+            r1.Display();
+            Console.WriteLine("===Rectangle_private=========");
+
+            Rectangle_internal r2 = new Rectangle_internal();
+            r2.length2 = 20;
+            r2.width2 = 10;
+            r2.Display();
+            Console.WriteLine("===Rectangle_internal=========");
+            Console.ReadKey();
+        }
+    }
+    class Rectangle_public
+    {
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("長度: {0}", length);
+            Console.WriteLine("寬度: {0}", width);
+            Console.WriteLine("面積: {0}", GetArea());
+        }
+
+
+    }
+
+    class Rectangle_private
+    {
+        private double length1;
+        private double width1;
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("請輸入長度:");
+            length1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("請輸入寬度:");
+            width1 = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetArea()
+        {
+            return length1 * width1;
+        }
+        public void Display()
+        {
+            Console.WriteLine("長度: {0}", length1);
+            Console.WriteLine("寬度: {0}", width1);
+            Console.WriteLine("面積: {0}", GetArea());
+        }
+
+
+    }
+
+    class Rectangle_internal
+    {
+        internal double length2;
+        internal double width2;
+
+        double GetArea()
+        {
+            return length2 * width2;
+        }
+        public void Display()
+        {
+            Console.WriteLine("長度: {0}", length2);
+            Console.WriteLine("寬度: {0}", width2);
+            Console.WriteLine("面積: {0}", GetArea());
+        }
+
+    }
+}
---
+++ w3cschool_csharp/csharp-loops/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            
+            // while 循環
+            while (a < 20) 
+            {
+                Console.WriteLine("a = {0}", a);
+                a++;
+            }
+
+            // for 循環
+            for (int b = 100; b < 110; b += 1) 
+            {
+                Console.WriteLine("b = {0}", b);
+            }
+
+            // foreach 循環
+            int[] fibarray = new int[] { 0, 1, 1, 2, 3, 5, 8, 13};
+
+            foreach (int element in fibarray) 
+            {
+                System.Console.WriteLine(element);
+            }
+
+            // 類似 foreach 循環
+            for (int i = 0; i < fibarray.Length; i++) 
+            {
+                System.Console.WriteLine(fibarray[i]);
+            }
+
+            // do while 循環
+            int c = 10;
+
+            do
+            {
+                Console.WriteLine(" c = {0}", c);
+                c += 1;
+            } while (c < 20);
+
+            //break 語句
+            int d = 10;
+
+            while (d < 20) 
+            {
+                Console.WriteLine(" d = {0}", d);
+                d += 1;
+                if (d > 15)
+                {
+                    break;
+                }
+            }
+
+            //continue 語句
+            int e = 10;
+
+            do
+            {
+                if (e == 15)
+                {
+                    e += 1;
+                    continue;
+                }
+                Console.WriteLine("e = {0}", e);
+                e++;
+            } while (e < 20);
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
