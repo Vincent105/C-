5cd2288074bb8a06299daa7b7f4b9925dff3c8f8
vincent105 <vincent1050917@gmail.com>
2021-10-25 21:37:01 +0800

    commit

---
+++ w3cschool_csharp/12_csharp-array/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_csharp_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10];
+            int i, j;
+
+            /*½á­È*/
+            for (i = 0; i < 10; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            /*³X°Ý°}¦C*/
+            for (j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Element[{0}]={1}", j, n[j]);
+            }
+
+            double[] balance = new double[10];
+            balance[0] = 45000;
+
+            double[] balance1 = { 220.1, 330, 2 };
+            Console.WriteLine("{0}", balance1[0]);
+
+            int[] marks = new int[5] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("{0}", marks[4]);
+
+            Console.ReadKey();
+        }
+    }
+}
