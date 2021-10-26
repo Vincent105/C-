8cd60e7526969742a60f146370549d1be1b568dd
Vincent105 <vincent1050917@gmail.com>
2021-10-26 09:08:54 +0800

    1026

--- w3cschool_csharp/12_csharp-array/Program.cs
+++ w3cschool_csharp/12_csharp-array/Program.cs
@@ -25,12 +25,16 @@ namespace _12_csharp_array
                 Console.WriteLine("Element[{0}]={1}",j,n[j]);
             }
 
+            /*賦值給陣列*/
             double[] balance = new double[10];
             balance[0] = 4500;
+            Console.WriteLine("{0}", balance[0]);
 
+            /*聲明陣列時同時賦值給陣列*/
             double[] balance1 = { 220.1, 330, 2 };
             Console.WriteLine("{0}", balance1[0]);
-
+            
+            /*建立時初始化陣列*/
             int[] marks = new int[5] { 1, 2, 3, 4, 5 };
             Console.WriteLine("{0}", marks[4]);
 
