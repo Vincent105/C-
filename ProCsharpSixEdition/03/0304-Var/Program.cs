87091788bca07a12356abd323ceb215446323813
vincent105 <vincent1050917@gmail.com>
2021-11-20 22:23:41 +0800

    11

--- ProCsharpSixEdition/03/0304-Var/Program.cs
+++ ProCsharpSixEdition/03/0304-Var/Program.cs
@@ -11,6 +11,7 @@ namespace _0304_Var
         static void Main(string[] args)
         {
             LocalVarDeclarations();
+            UseDateAndTime();
             Console.ReadKey();
         }
 
@@ -34,6 +35,38 @@ namespace _0304_Var
             Console.WriteLine("{0}", 12.Equals(23));
             Console.WriteLine("{0}", 12.ToString());
             Console.WriteLine("{0}", 12.GetType());
+
+            Console.WriteLine("{0}", int.MaxValue);
+            Console.WriteLine("{0}", int.MinValue);
+            Console.WriteLine("{0}", double.MaxValue);
+            Console.WriteLine("{0}", double.MinValue);
+            Console.WriteLine("{0}", double.Epsilon);
+            Console.WriteLine("{0}", double.PositiveInfinity);
+            Console.WriteLine("{0}", double.NegativeInfinity);
+
+            Console.WriteLine("{0}",bool.TrueString);
+            Console.WriteLine("{0}",bool.FalseString);
+
+            //型別強制轉換
+            Console.WriteLine("=> Data type parsing:");
+            bool b11 = bool.Parse("True");
+            Console.WriteLine("{0}", b11);
+            Console.WriteLine("{0}", b11.GetType());
+
+            double i11 = double.Parse("99.88");
+            Console.WriteLine("{0}", i11);
+            Console.WriteLine("{0}", i11.GetType());
+        }
+
+        static void UseDateAndTime()
+        {
+            Console.WriteLine("=> Dates and Times:");
+
+            DateTime dt = new DateTime(2021,11,19,01,01,01);
+            Console.WriteLine("{0},{1}",dt.Date,dt.DayOfWeek);
+
+            dt = dt.AddDays(30);
+            Console.WriteLine("{0}",dt.Day);
         }
     }
 }
